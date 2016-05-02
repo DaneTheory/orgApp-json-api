f03289c2c39b0c1ba88487ca9ae6c8577338c623
Branden Dane <branden@danetheory.com>
2016-05-02 06:43:06 -0400

    fix

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var jsonServer = require('json-server')
 var server = jsonServer.create()
 var router = jsonServer.router('db.json')
-var middlewares = jsonServer.defaults()
+var middlewares = jsonServer.defaults();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// app.configure(function() {
+//     app.use(express.bodyParser());
+// });
+
+
+server.use('/api', router)
 
 server.use(middlewares)
 server.use(router)
+
+app.listen(8000, function () {
+  console.log('Express is up')
+})
+
 
 server.listen(8080, function () {
   console.log('JSON Server is running')
